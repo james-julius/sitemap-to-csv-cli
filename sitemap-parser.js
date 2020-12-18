5c66ebeaae69af2ea322c9bef9301ec5dd4cbb8c
james-julius <jamesdarby7@gmail.com>
2020-12-18 03:11:51 +0000

    Created bassis for first npm package

---
+++ sitemap-parser.js
@@ -0,0 +1,43 @@
+const Sitemapper = require('sitemapper')
+const { convertArrayToCSV } = require('convert-array-to-csv')
+const stringify = require('csv-stringify')
+const fs = require('fs')
+
+
+/** Package features:
+ ********************
+ *  * Multiple inputs
+ *  * Timer between multiple starts
+ *  * input filename
+ *  * cli for input url + filename
+ */
+
+const csvHeader = ['url'];
+
+
+const sitemapper = new Sitemapper();
+sitemapper.timeout = 5000;
+
+
+sitemapper
+  .fetch("https://www.allrecipes.com/sitemaps/recipe/1/sitemap.xml")
+  // Needs to be in array format for the stringify function to work
+  .then(({ url, sites }) => {
+    for (let n = 0; n < sites.length; n++) {
+      console.log("Processing url #" + n);
+      sites[n] = [sites[n]];
+    }
+    stringify(sites, (err, output) => {
+      fs.writeFile("allrecipes-sitemap-1.csv", output, "utf8", (err) => {
+        if (err) {
+          console.log(
+            "Some error occured - file either not saved or corrupted file saved."
+          );
+        } else {
+          console.log("It's saved!");
+        }
+      });
+    });
+    console.log(`url:${url}`);
+  })
+  .catch((error) => console.log(error));
